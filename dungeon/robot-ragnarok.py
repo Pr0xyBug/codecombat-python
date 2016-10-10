39938ceff6e19474bb18bdb9926ae75b0313eee3
Pr0xyBug <Pr0xyBug@protonmail.com>
2016-10-09 23:44:58 -0400

    Create robot-ragnarok.py

---
+++ dungeon/robot-ragnarok.py
@@ -0,0 +1,13 @@
+# Enact the Konami Code
+
+while True:
+    hero.moveUp()
+    hero.moveUp()
+    hero.moveDown()
+    hero.moveDown()
+    hero.moveLeft()
+    hero.moveRight()
+    hero.moveLeft()
+    hero.moveRight()
+    # B, A, Start
+    break
